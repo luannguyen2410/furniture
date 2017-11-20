e3305caa0603f1282e0ce00728252e1709485914
DESKTOP-ELA18DE\luann <luannguyen2410@gmail.com>
2017-11-20 10:50:39 +0700

    upadte facebook

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -179,19 +179,22 @@
         <div class="col-md-12">
           <div class="row">
 			      <div class="footer-col col-md-4">
-              <h6>FANPAGE FACEBOOK</h6>
-              <div class="fb-page">
-              <img class="img-fluids" style="width:100%;height:220px" src="{{ URL::asset('img/slide1.png')}}">
-              <div class="img-overlay">
-                <a href="https://www.facebook.com/pg/noithatpqmax/about/?ref=page_internal" target="_blank" title="Facebook" class="btn btn-facebook btn-lg"><i class="fa fa-facebook fa-fw"></i> Thích trang</a>
-                <a href="https://www.facebook.com/sharer/sharer.php?s=100&p[url]=https://www.facebook.com/pg/noithatpqmax/about/?ref=page_internal&p[images][0]=&p[title]=Title%20Goes%20Here&p[summary]=Description%20goes%20here!" target="_blank" onclick="window.open(this.href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=250'); return false"><button type="button" class="btn btn-facebook btn-lg"><i class="fa fa-facebook fa-2"></i> Chia sẻ</button></a>
-              </div>
-              </div>
-              <!-- <div class="fb-page" 
-              data-href="https://web.facebook.com/noithatpqmax/"
-              data-hide-cover="false"
-              data-show-facepile="false"></div> -->
+            <h6>FANPAGE FACEBOOK</h6>
+            <div class="fb-page">    
+            <img class="img-fluid"  src="{{ URL::asset('img/slide1.png')}}">
+            <div class="img-overlay">
+            <div class="fb-like" style ="background-color:white; panding-top:10px" data-href="https://www.facebook.com/pg/noithatpqmax/about/?ref=page_internal" data-width="385" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
             </div>
+        </div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.11';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+          </div>  
             <div class="footer-col col-md-4">
               <div class="container">
 				<h6 class="text-center">Liên hệ với chúng tôi</h6>
